4ce4161b9447385a8171be975eaa9d1a165551a9
bchamarthi <brchamarthi29@gmail.com>
2018-07-26 20:15:12 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,39 @@
 # book.rb
 # Add you Book class here
+class Book 
+  
+  def initialize(title)
+    @title = title 
+  end 
+
+  def title
+    @title 
+  end
+  
+  def author 
+    @author 
+  end 
+  
+  def page_count = (num)
+    @page_count = num
+  end 
+  
+  def page_count 
+    @page_count 
+  end 
+  
+  def genre 
+    @genre 
+  end 
+  
+  def turn_page
+    puts "Flipping the page... wow you read fast!"
+  end 
+  
+
+end 
+
+ book.new("And Then There Were None")
+ 
+ book.new("Some Title")
+book.turn_page  
